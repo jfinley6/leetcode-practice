e475235fc48e0baa0581ada115ad3dccbab19b52
johnfinley <johnfinleybremerton@gmail.com>
2024-08-23 12:37:27 -0600

    Update: added solution for rotate problem

--- arrays_and_strings.py
+++ arrays_and_strings.py
@@ -208,8 +208,21 @@ class Solution:
                 j += 1
                 UP_WALL += 1
                 direction = RIGHT
-                
+
         return ans
+    
+    # You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j] 
+
+        for i in range(n):
+            matrix[i].reverse()
+
+        return matrix
     
 # Create an instance of the Solution class
 solution = Solution()
@@ -274,6 +287,12 @@ solution = Solution()
 
 # spiralOrder cases
 # Outputs: [1, 2, 3, 6, 9, 8, 7, 4, 5]
-print(solution.spiralOrder([[1,2,3],[4,5,6],[7,8,9]]))
+# print(solution.spiralOrder([[1,2,3],[4,5,6],[7,8,9]]))
 # Outputs: [1,2,3,4,8,12,11,10,9,5,6,7]
-print(solution.spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
+# print(solution.spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
+
+# rotate cases
+# Output: [[7,4,1],[8,5,2],[9,6,3]]
+print(solution.rotate([[1,2,3],[4,5,6],[7,8,9]]))
+# Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+print(solution.rotate([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]))
